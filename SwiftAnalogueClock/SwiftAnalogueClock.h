ca41659f409e99f966e1db3327dc83e703b7a620
Priyesh <priyesh@osper.com>
2021-01-26 20:02:40 +0530

    Initial Commit

---
+++ SwiftAnalogueClock/SwiftAnalogueClock.h
@@ -0,0 +1,18 @@
+//
+//  SwiftAnalogueClock.h
+//  SwiftAnalogueClock
+//
+//  Created by Priyesh on 26/01/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftAnalogueClock.
+FOUNDATION_EXPORT double SwiftAnalogueClockVersionNumber;
+
+//! Project version string for SwiftAnalogueClock.
+FOUNDATION_EXPORT const unsigned char SwiftAnalogueClockVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftAnalogueClock/PublicHeader.h>
+
+
