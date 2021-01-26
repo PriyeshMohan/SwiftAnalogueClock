b8f8981efff272f6997b719a287cc17d72ab0803
Priyesh <priyesh@osper.com>
2021-01-26 20:10:54 +0530

    Initial project setup

---
+++ Shared/SwiftAnalogueClockExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAnalogueClockExampleApp.swift
+//  Shared
+//
+//  Created by Priyesh on 26/01/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAnalogueClockExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
